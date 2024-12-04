bc08c20c7e3438105a1148545e55c18ee25b018a
aheldis <ehsanihani30@yahoo.com>
2024-12-03 18:44:18 -0800

    added q-ffl and projection

--- fedprox.py
+++ fedprox.py
@@ -1,32 +1,57 @@
 from copy import deepcopy
 from scripts import loss_dataset, accuracy_dataset, local_learning, loss_classifier, set_to_zero_model_weights
 import torch.optim as optim
+import torch
 
-def average_models(model, clients_models_hist:list , weights:list):
 
-
+def average_models(model, prev, clients_models_hist: list, weights: list, first, project):
     """Creates the new model of a given iteration with the models of the other
     clients"""
-    
-    new_model=deepcopy(model)
+
+    new_model = deepcopy(model)
     set_to_zero_model_weights(new_model)
+    pre_hist = None
 
-    for k,client_hist in enumerate(clients_models_hist):
-        
+    if project:
         for idx, layer_weights in enumerate(new_model.parameters()):
+            mini = None
+            for k, client_hist in enumerate(clients_models_hist):
+                if not first:
+                    pre_hist = prev[k]
+                    dif = torch.linalg.norm(client_hist[idx].data - pre_hist[idx].data)
+                    if mini is None:
+                        mini = dif
+                    else:
+                        mini = min(mini, dif)
+            if mini:
+                mini = torch.sqrt(mini)
+            for k, client_hist in enumerate(clients_models_hist):
+                if mini:
+                    pre_hist = prev[k]
+                    dif = client_hist[idx].data - pre_hist[idx].data
+                    hist = pre_hist[idx].data + dif / torch.sqrt(torch.linalg.norm(dif)) * mini
+                    contribution = hist * weights[k]
+                else:
+                    contribution = client_hist[idx].data * weights[k]
+                # contribution = client_hist[idx].data * weights[k]
+                layer_weights.data.add_(contribution)
 
-            contribution=client_hist[idx].data*weights[k]
-            layer_weights.data.add_(contribution)
-            
+
+    else:
+        for k, client_hist in enumerate(clients_models_hist):
+            for idx, layer_weights in enumerate(new_model.parameters()):
+                contribution = client_hist[idx].data * weights[k]
+                layer_weights.data.add_(contribution)
+
     return new_model
 
 
-def FedProx(model, training_sets:list, n_iter:int, testing_sets:list, mu=0, 
-    file_name="test", epochs=5, lr=10**-2, decay=1):
+def FedProx(model, training_sets: list, n_iter: int, testing_sets: list, mu=0,
+            file_name="test", epochs=5, lr=10 ** -2, decay=1, project=True, q=0):
     """ all the clients are considered in this implementation of FedProx
     Parameters:
         - `model`: common structure used by the clients and the server
-        - `training_sets`: list of the training sets. At each index is the 
+        - `training_sets`: list of the training sets. At each index is the
             training set of client "index"
         - `n_iter`: number of iterations the server will run
         - `testing_set`: list of the testing sets. If [], then the testing
@@ -35,75 +60,71 @@ def FedProx(model, training_sets:list, n_iter:int, testing_sets:list, mu=0,
         - `epochs`: number of epochs each client is running
         - `lr`: learning rate of the optimizer
         - `decay`: to change the learning rate at each iteration
-    
+
     returns :
-        - `model`: the final global model 
+        - `model`: the final global model
     """
-        
-    loss_f=loss_classifier
-    
-    #Variables initialization
-    K=len(training_sets) #number of clients
-    n_samples=sum([len(db.dataset) for db in training_sets])
-    weights=([len(db.dataset)/n_samples for db in training_sets])
-    print("Clients' weights:",weights)
-    
-    
-    loss_hist=[[float(loss_dataset(model, dl, loss_f).detach()) 
-        for dl in training_sets]]
-    acc_hist=[[accuracy_dataset(model, dl) for dl in testing_sets]]
-    server_hist=[[tens_param.detach().numpy() 
-        for tens_param in list(model.parameters())]]
+
+    loss_f = loss_classifier
+
+    # Variables initialization
+    K = len(training_sets)  # number of clients
+    n_samples = sum([len(db.dataset) for db in training_sets])
+    weights = ([len(db.dataset) / n_samples for db in training_sets])
+    print("Clients' weights:", weights)
+
+    loss_hist = [[float(loss_dataset(model, dl, loss_f).detach())
+                  for dl in training_sets]]
+    acc_hist = [[accuracy_dataset(model, dl) for dl in testing_sets]]
+    server_hist = [[tens_param.detach().numpy()
+                    for tens_param in list(model.parameters())]]
     models_hist = []
-    
-    
-    server_loss=sum([weights[i]*loss_hist[-1][i] for i in range(len(weights))])
-    server_acc=sum([weights[i]*acc_hist[-1][i] for i in range(len(weights))])
+
+    server_loss = sum([weights[i] * loss_hist[-1][i] for i in range(len(weights))])
+    server_acc = sum([weights[i] * acc_hist[-1][i] for i in range(len(weights))])
     print(f'====> i: 0 Loss: {server_loss} Server Test Accuracy: {server_acc}')
-    
+    clients_params = []
+
     for i in range(n_iter):
-        
-        clients_params=[]
-        clients_models=[]
-        clients_losses=[]
-        
+        prev_clients_params = [deepcopy(_) for _ in clients_params]
+        clients_params = []
+        clients_models = []
+        clients_losses = []
+
         for k in range(K):
-            
-            local_model=deepcopy(model)
-            local_optimizer=optim.SGD(local_model.parameters(),lr=lr)
-            
-            local_loss=local_learning(local_model,mu,local_optimizer,
-                training_sets[k],epochs,loss_f)
-            
+            local_model = deepcopy(model)
+            local_optimizer = optim.SGD(local_model.parameters(), lr=lr)
+
+            local_loss = local_learning(local_model, mu, local_optimizer,
+                                        training_sets[k], epochs, loss_f, q)
+
             clients_losses.append(local_loss)
-                
-            #GET THE PARAMETER TENSORS OF THE MODEL
-            list_params=list(local_model.parameters())
-            list_params=[tens_param.detach() for tens_param in list_params]
-            clients_params.append(list_params)    
+
+            # GET THE PARAMETER TENSORS OF THE MODEL
+            list_params = list(local_model.parameters())
+            list_params = [tens_param.detach() for tens_param in list_params]
+            clients_params.append(list_params)
             clients_models.append(deepcopy(local_model))
-        
-        
-        #CREATE THE NEW GLOBAL MODEL
-        model = average_models(deepcopy(model), clients_params, 
-            weights=weights)
+
+        # CREATE THE NEW GLOBAL MODEL
+        model = average_models(deepcopy(model), prev_clients_params, clients_params,
+                               weights=weights, first=(i == 0), project=project)
         models_hist.append(clients_models)
-        
-        #COMPUTE THE LOSS/ACCURACY OF THE DIFFERENT CLIENTS WITH THE NEW MODEL
-        loss_hist+=[[float(loss_dataset(model, dl, loss_f).detach()) 
-            for dl in training_sets]]
-        acc_hist+=[[accuracy_dataset(model, dl) for dl in testing_sets]]
 
-        server_loss=sum([weights[i]*loss_hist[-1][i] for i in range(len(weights))])
-        server_acc=sum([weights[i]*acc_hist[-1][i] for i in range(len(weights))])
+        # COMPUTE THE LOSS/ACCURACY OF THE DIFFERENT CLIENTS WITH THE NEW MODEL
+        loss_hist += [[float(loss_dataset(model, dl, loss_f).detach())
+                       for dl in training_sets]]
+        acc_hist += [[accuracy_dataset(model, dl) for dl in testing_sets]]
 
-        print(f'====> i: {i+1} Loss: {server_loss} Server Test Accuracy: {server_acc}')
-        
+        server_loss = sum([weights[i] * loss_hist[-1][i] for i in range(len(weights))])
+        server_acc = sum([weights[i] * acc_hist[-1][i] for i in range(len(weights))])
 
-        server_hist.append([tens_param.detach().cpu().numpy() 
-            for tens_param in list(model.parameters())])
-        
-        #DECREASING THE LEARNING RATE AT EACH SERVER ITERATION
-        lr*=decay
-            
+        print(f'====> i: {i + 1} Loss: {server_loss} Server Test Accuracy: {server_acc}')
+
+        server_hist.append([tens_param.detach().cpu().numpy()
+                            for tens_param in list(model.parameters())])
+
+        # DECREASING THE LEARNING RATE AT EACH SERVER ITERATION
+        lr *= decay
+
     return model, loss_hist, acc_hist
--- main.py
+++ main.py
@@ -1,55 +1,57 @@
-import argsparse
+import argparse
 from data import get_MNIST, plot_samples
 from model import CNN
 from fedprox import FedProx
 import matplotlib.pyplot as plt
 
 
-def plot_acc_loss(title:str, loss_hist:list, acc_hist:list):
+def plot_acc_loss(title: str, loss_hist: list, acc_hist: list):
     plt.figure()
-    
+
     plt.suptitle(title)
 
-    plt.subplot(1,2,1)
-    lines=plt.plot(loss_hist)
+    plt.subplot(1, 2, 1)
+    lines = plt.plot(loss_hist)
     plt.title("Loss")
-    plt.legend(lines,["C1", "C2", "C3"])
+    plt.legend(lines, ["C1", "C2", "C3"])
 
-    plt.subplot(1,2,2)
-    lines=plt.plot(acc_hist )
+    plt.subplot(1, 2, 2)
+    lines = plt.plot(acc_hist)
     plt.title("Accuracy")
     plt.legend(lines, ["C1", "C2", "C3"])
-    
+    plt.savefig(title)
+
 
 if __name__ == "__main__":
-    parser = argsparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
     parser.add_argument("--model", type=str, default="basic")
     parser.add_argument("--dataset", type=str, default="mnist")
-    parser.add_argument("--setting", type=str, default="non-iid")
-    parser.add_argument("--n_iter", type=int, default=10)
+    parser.add_argument("--setting", type=str, default="iid")
+    parser.add_argument("--n_iter", type=int, default=20)
     parser.add_argument("--n_clients", type=int, default=3)
     parser.add_argument("--batch_size", type=int, default=25)
     parser.add_argument("--lr", type=float, default=1e-1)
     parser.add_argument("--epochs", type=int, default=2)
     parser.add_argument("--visualize", type=bool, default=False)
+    parser.add_argument("--project", type=bool, default=True)
     args = parser.parse_args()
 
     # TODO: Add other models: basic ResNetand other baselines (VGG, AlexNet, ...)
     if args.model == "basic":
         model_0 = CNN()
-    
+
     # TODO: Add other datasets: CIFAR10, CIFAR100, CelebA (probably)
     if args.dataset == "mnist" and args.setting == "iid":
         mnist_train_dls, mnist_test_dls = get_MNIST(
-            "iid", n_samples_train=200, n_samples_test=100, 
-            n_clients=args.n_clients, batch_size=args.n_clients, shuffle =True
+            "iid", n_samples_train=200, n_samples_test=100,
+            n_clients=args.n_clients, batch_size=args.n_clients, shuffle=True
         )
 
     elif args.dataset == "mnist" and args.setting == "non-iid":
         mnist_train_dls, mnist_test_dls = get_MNIST(
-            "non_iid", n_samples_train=200, n_samples_test=100, 
-            n_clients=args.n_clients, batch_size=args.batch_size, shuffle =True
+            "non_iid", n_samples_train=200, n_samples_test=100,
+            n_clients=args.n_clients, batch_size=args.batch_size, shuffle=True
         )
 
     if args.visualize:
@@ -58,16 +60,16 @@ if __name__ == "__main__":
         plot_samples(next(iter(mnist_train_dls[2])), 0, "Client 3")
 
     # FedAvg
-    model_f, loss_hist_FA, acc_hist_FA = FedProx( 
-        model_0, mnist_train_dls, args.n_iter, 
-        mnist_test_dls, epochs=args.epochs, lr=args.lr, mu=0
+    model_f, loss_hist_FA, acc_hist_FA = FedProx(
+        model_0, mnist_train_dls, args.n_iter,
+        mnist_test_dls, epochs=args.epochs, lr=args.lr, mu=0, project=args.project
     )
 
     # FedProx
-    model_f, loss_hist_FP, acc_hist_FP = FedProx( 
-        model_0, mnist_train_dls, args.n_iter, 
-        mnist_test_dls, epochs=args.epochs, lr=args.lr, mu=0.3)
+    model_f, loss_hist_FP, acc_hist_FP = FedProx(
+        model_0, mnist_train_dls, args.n_iter,
+        mnist_test_dls, epochs=args.epochs, lr=args.lr, mu=0.3, project=args.project)
 
     # comparison
-    plot_acc_loss("FedAvg MNIST-iid", loss_hist_FA, acc_hist_FA)
+    plot_acc_loss("ProjFedAvg MNIST-iid", loss_hist_FA, acc_hist_FA)
 
